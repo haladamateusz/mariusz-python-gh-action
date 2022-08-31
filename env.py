96fecc629e265cc7bc4d22a35d27f27260883e5a
Mateusz Halada <mateusz.halada@ch.gudel.com>
2022-08-31 15:02:51 +0200

    change endpoint for sending text messages

--- env.py
+++ env.py
@@ -2,7 +2,7 @@ import os
 URL = 'https://mariusz-backend.herokuapp.com'
 LOGIN_URL = URL + '/login'
 SEND_EMAIL_URL = URL + '/email'
-SEND_SMS_URL = URL + '/sms'
+SEND_SMS_URL = URL + '/text-message'
 SAVE_INVOICE_LOG_URL = URL + '/invoice-log'
 
 CREDENTIALS = {
